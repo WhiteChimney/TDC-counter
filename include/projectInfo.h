69904c228644266b31ceccdcbdfb9ca633825415
WhiteChimney <white8chimney@gmail.com>
2023-08-11 13:20:56 +0800

    Revert "Update projectInfo.h"
    This reverts commit 0d8b4cc3363118a5c8edd120ffdf8bb1efa92165.

--- include/projectInfo.h
+++ include/projectInfo.h
@@ -3,7 +3,7 @@
 
 #define PROJECT_VERSION_0 "3"
 #define PROJECT_VERSION_1 "0"
-#define PROJECT_VERSION_2 "4"
+#define PROJECT_VERSION_2 "3"
 #define PROJECT_RELEASE_PAGE "https://github.com/WhiteChimney/TDC-counter/releases"
 
 #endif // PROJECTINFO_H
