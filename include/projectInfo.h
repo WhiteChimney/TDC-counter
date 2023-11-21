9b70e11e82bf00b55a954ea5232de5a031c3ed15
WhiteChimney <white8chimney@gmail.com>
2023-11-21 22:58:09 +0800

    更新版本号

--- include/projectInfo.h
+++ include/projectInfo.h
@@ -3,7 +3,7 @@
 
 #define PROJECT_VERSION_0 "3"
 #define PROJECT_VERSION_1 "1"
-#define PROJECT_VERSION_2 "0"
+#define PROJECT_VERSION_2 "1"
 #define PROJECT_RELEASE_PAGE "https://github.com/WhiteChimney/TDC-counter/releases"
 
 #endif // PROJECTINFO_H
